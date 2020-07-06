d82670544a4dc32fa5ba3a34ad0a3263b7e0a2aa
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-06 16:55:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,5 +23,6 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do
     @num_1 = params[:num1].to_i
     @num_2 = params[:num2].to_i
-    ""
+    @total = @num_1 * @num_2
+    "#{@total}"
 end
